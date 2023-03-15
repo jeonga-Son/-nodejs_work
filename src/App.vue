d2e473173a32c732682b379cdeca6d3f386ad35d
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-15 15:37:30 +0900

    use ref, reactive

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="name">{{greeting('MetaNet')}}</div>
+  <div class="name">
+    {{name}}
+  </div>
+  <input type="text" value="name">
+  <button 
+    class="btn btn-primary"
+    v-on:click="updateName"
+  >
+    Click
+  </button>
 </template>
 
 <script>
+import { ref } from 'vue' ;
 export default {
  setup(){
-  const name = "Hello MetaNet!";
-  const greeting = (name) => {
-    return 'Hello! ' + name;
+  const name = ref('kosa');
+
+  const updateName = () => {
+    name.value = 2222222;
+    console.log(name);
   }
+ 
+
 // 값을 리턴해주지 않으면 오류가 난다.
   return{
     name,
-    greeting,
+    updateName,
   }
  }
 }
