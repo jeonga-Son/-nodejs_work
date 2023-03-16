0c94288ecff21c00133edd4b6906b6327ce43a1b
“jeonga-Son” <jjeong0106@gmail.com>
2023-03-16 17:22:37 +0900

    add pagination feature

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,58 @@
-<template>  
+<template>    
   <div class="container">
     <h2>To-Do List</h2>
-
     <input
       class="form-control" 
       type="text" 
       v-model="searchText"
       placeholder="Search"
     >
-
-    <br>
-
+    <hr>
     <TodoSimpleForm @add-todo="addTodo"/>
-
-    <div style="color: red">
-      {{error}}
-    </div>
-
+    <div style="color: red">{{error}}</div>
+    
     <div v-if="!todos.length">
       추가된 Todo가 없습니다.
     </div>
 
     <div v-if="!filteredTodos.length">
-      There is nothing to display
-    </div>
+      There is noting to display  
+    </div>    
 
     <TodoList :todos="filteredTodos"
-              @toggle-todo="toggleTodo" 
-              @delete-todo="deleteTodo"/>
+              @toggle-todo="toggleTodo"
+              @delete-todo="deleteTodo" />
 
+    <br>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li
+          v-if="currentPage !== 1" 
+          class="page-item">
+          <a class="page-link"  @click="getTodos(currentPage - 1)">Previous</a>
+        </li>
+        <li
+          v-for="page in numberOfPages"
+          :key="page"
+          class="page-item"
+          :class="currentPage === page ? 'active' : ''">
+          <a class="page-link" @click="getTodos(page)">
+            {{page}}
+          </a>
+        </li>        
+        <li 
+          v-if="numberOfPages != currentPage"
+          class="page-item">
+          <a class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>          
+   
   </div>   
 </template>
 
 <script>
-import { ref , computed} from 'vue';
+import { ref, computed } from 'vue';
 import axios from "axios";
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
@@ -42,76 +61,99 @@ export default {
     TodoSimpleForm,
     TodoList
   },
-  setup(){
-      
-    const todos = ref([]);   
+  setup(){      
+    const todos = ref([]); 
     const searchText = ref('');
     const error = ref('');
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
+
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value/limit);
+    });
 
     const filteredTodos = computed(() => {
-      if(searchText.value) {
+      if(searchText.value){
         return todos.value.filter(todo => {
           return todo.subject.includes(searchText.value);
         });
       }
 
-      return todos.value;
+      return todos.value
     });
 
-    // 삭제 구현
     const deleteTodo = async (index) => {
-      error.value ='';
+      error.value = '';
       const id = todos.value[index].id;
       try{
-          await axios.delete('http://localhost:3000/todos/' + id); 
-          todos.value.splice(index, 1);
-      } catch(err) {
-        console.log(error);
-        error.value = 'Something went wrong';
-      }
-      
+        await axios.delete('http://localhost:3000/todos/'+id);
+        todos.value.splice(index, 1);
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
+      } 
     }   
 
-    const getTodos = async () => {
-      error.value ='';
-       try{
-          const res = await axios.get('http://localhost:3000/todos');  
-          todos.value = res.data;
-        } catch(err) {
-          console.log(error);
-          error.value = 'Something went wrong';
-        }
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
+      error.value = '';
+      try{
+        const res = await axios.get(
+          `http://localhost:3000/todos?_page=${page}&_limit=${limit}`);
+
+        numberOfTodos.value = res.headers['x-total-count'];  
+        //console.log(res.headers['x-total-count']);
+        todos.value = res.data;
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
+      }
     }
 
     getTodos();
 
     const addTodo = async (todo) => {
-      error.value ='';
+      error.value = '';
       try{
-          const res = await axios.post('http://localhost:3000/todos', { // 테이블형식으로 데이터를 post하면 추가된다.
+        const res = await axios.post('http://localhost:3000/todos', {
           subject: todo.subject,
           completed: todo.completed
         });
-          todos.value.push(res.data); // 배열안에 데이터값을 넣는 것
-      } catch(err) {
-        console.log(error);
-        error.value = 'Something went wrong';
-      }
+        todos.value.push(res.data);
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
+      }     
     }
 
-    const toggleTodo = (index) => {     
-      todos.value[index].completed = !todos.value[index].completed;      
+    const toggleTodo = async (index) => {  
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+        await axios.patch('http://localhost:3000/todos/'+id, {
+          completed: !todos.value[index].completed
+        });
+        todos.value[index].completed = !todos.value[index].completed;  
+      }catch(err){
+        console.log(err);
+        error.value = 'Someting went wrong';
+      } 
     }
     
     return{      
       todos,      
       deleteTodo,
       addTodo,
-      toggleTodo,
+      toggleTodo, 
       searchText,
-      filteredTodos,
-      error,
+      filteredTodos, 
+      error,  
       getTodos,
+      numberOfTodos,
+      limit,
+      currentPage,
+      numberOfPages, 
     }
   }  
 }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,7 +7,7 @@
             <div class="form-check flex-grow-1">
                 <input class="form-check-input"
                         type="checkbox"
-                        :value="todo.completed"
+                        :checked="todo.completed"
                         @change="toggleTodo(index)">
                 <label class="form-check-label"
                         :class="{todo: todo.completed}">
@@ -32,24 +32,20 @@ export default {
             type: Array,
             required: true
         }
-    }, 
-    emits: ['toggle-todo','delete-todo'], // emit을 여러번 사용하기 때문에 목록으로 명시해준다. 
+    },
+    emits: ['toggle-todo', 'delete-todo'],
     setup(props, {emit}){
         const toggleTodo = (index) => {
             emit('toggle-todo', index);
         }
 
-        // app.vue에 리스트가 있기 때문에 함수를 가져올 수 없다.
-        // 인덱스 값을 전달하여 삭제를 한다.
-        // 자식 -> 부모에게 삭제 버튼을 클릭한 사실을 전달한다.
         const deleteTodo = (index) => {
             emit('delete-todo', index);
         }
-        
 
         return {
             toggleTodo,
-            deleteTodo
+            deleteTodo,
         }
     }   
 }
